6219dc9a2ac72e6940bca44f3e98bf26dde921c7
Jochelle Viray <Joviray@users.noreply.github.com>
2023-06-20 14:39:14 -0700

    Dynamic Route Data

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,10 +1,12 @@
+import { prisma } from '@/lib/prisma';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import NextAuth from "next-auth/next";
 import type { NextAuthOptions } from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
 import CredentialsProvider from 'next-auth/providers/credentials'
 
 export const authOptions: NextAuthOptions = {
-    secret: process.env.SECRET!,
+    adapter: PrismaAdapter(prisma),
     providers: [
         GithubProvider({
             clientId: process.env.GITHUB_ID!,
---
+++ app/users/[id]/page.tsx
@@ -0,0 +1,34 @@
+import FollowButton from '@/components/FollowButton';
+import { prisma } from '@/lib/prisma';
+import { Metadata } from '@next';
+
+interface Props {
+    params: {
+        id: string;
+    };
+}
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+    const user = await prisma.user.findUnique({ where: { id: params.id } });
+    return { title: `User profile of ${user?.name}` };
+}
+
+
+export default async function UsersProfile({ params}: Props){
+    const users = await prisma.user.findUnique({ where: { id: params.id }});
+    const { name, bio, image } = users ?? {};
+
+    return (
+        <div>
+            <h1>{name}}</h1>
+            <img 
+               width={300}
+               src={image ?? '/mememan.webp'}
+               alt = {`${name}'s profile`}
+               />
+               <h3>Bio</h3>
+               <p>{bio}</p>    
+
+        </div>
+    );
+}
