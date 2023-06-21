cdf8daa841d52ee295549bee5f2604a51560ebbd
Jochelle Viray <Joviray@users.noreply.github.com>
2023-06-21 13:24:45 -0700

    adding route
    fix the route.ts next time

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -3,9 +3,8 @@ import styles from './page.module.css';
 import { prisma } from '@/lib/prisma';
 
 export default async function Users(){
-    throw new Error('This is an error');
-    const users = await prisma.user.findMany();
     
+    const users = await prisma.user.findMany();
     return(
         <div className = {styles.grid}>
             {users.map((user) => {
