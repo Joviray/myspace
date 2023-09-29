8eab882ad5561c467b2c0673b3ebe780e451937a
Jochelle <j3241@live.com>
2023-09-28 21:19:45 -0700

    look at the json file

--- app/calendar/page.tsx
+++ app/calendar/page.tsx
@@ -1,23 +1,46 @@
-import React from react
+import React from 'react'
+import { formatDate } from '@fullcalendar/core'
 import FullCalendar from '@fullcalendar/react'
-import { Metadata } from "next";
-
-
-export const metadata: Metadata = {
-    title: 'Calendar',
-    description: 'you can find the rooms that are available here'
-};
+import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import interactionPlugin from '@fullcalendar/interaction'
+import { INITIAL_EVENTS, createEventId } from './event-utils'
 
 
 
-export default async function Calendar(){
+export default class page extends React.Component {
+  
+    state = {
+        weekendsVisible: true,
+        currentEvents: []
+    }
+    render(){
+        return(
+            <div className='demo-app'>
+                {this.renderSidebar()}
+                <div className='demo-app-main'>
+                    <FullCalendar
+                        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+                        headerToolbar={{
+                            left: 'prev,next today',
+                            center: 'title',
+                            right: 'dayGridMonth,timeGridWeek,timeGridDay'
+                        }}
+                        initialView='dayGridMonth'
+                        editable={true}
+                        selectable={true}
+                        selectMirror={true}
+                        dayMaxEvents={true}
+                        weekends={this.state.weekendsVisible}
+                        initialEvents={INITIAL_EVENTS} // alternatively, use the `events` setting to fetch from a feed
+                        select={this.handleDateSelect}
+                        eventContent={renderEventContent} // custom render function
+                        eventClick={this.handleEventClick}
+                        eventsSet={this.handleEvents} // called after events are initialized/added/changed/removed
+                        />
+                        </div>
 
-    return (
-        <main>
-            <h1>Calendar</h1>
-            <p>
-                See where you can study 
-            </p>
-        </main>                                                                                                             
-    )
-};
+                </div>
+        )
+    }
+}
