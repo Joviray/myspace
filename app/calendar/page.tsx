484b7f476fdfda8aba5c0471f585aae33d1f902f
Jochelle Viray <Joviray@users.noreply.github.com>
2023-09-28 21:53:04 -0700

    Set calendar page as client component

--- app/calendar/page.tsx
+++ app/calendar/page.tsx
@@ -1,46 +1,30 @@
+'use client'
+
 import React from 'react'
 import { formatDate } from '@fullcalendar/core'
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
-import interactionPlugin from '@fullcalendar/interaction'
-import { INITIAL_EVENTS, createEventId } from './event-utils'
 
 
-
-export default class page extends React.Component {
-  
-    state = {
-        weekendsVisible: true,
-        currentEvents: []
-    }
-    render(){
-        return(
-            <div className='demo-app'>
-                {this.renderSidebar()}
-                <div className='demo-app-main'>
-                    <FullCalendar
-                        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-                        headerToolbar={{
-                            left: 'prev,next today',
-                            center: 'title',
-                            right: 'dayGridMonth,timeGridWeek,timeGridDay'
-                        }}
-                        initialView='dayGridMonth'
-                        editable={true}
-                        selectable={true}
-                        selectMirror={true}
-                        dayMaxEvents={true}
-                        weekends={this.state.weekendsVisible}
-                        initialEvents={INITIAL_EVENTS} // alternatively, use the `events` setting to fetch from a feed
-                        select={this.handleDateSelect}
-                        eventContent={renderEventContent} // custom render function
-                        eventClick={this.handleEventClick}
-                        eventsSet={this.handleEvents} // called after events are initialized/added/changed/removed
-                        />
-                        </div>
-
-                </div>
-        )
-    }
+export default function CalendarPage() {
+    return(
+        <div className='demo-app'>
+            <div className='demo-app-main'>
+                <FullCalendar
+                    plugins={[dayGridPlugin, timeGridPlugin]}
+                    headerToolbar={{
+                        left: 'prev,next today',
+                        center: 'title',
+                        right: 'dayGridMonth,timeGridWeek,timeGridDay'
+                    }}
+                    initialView='dayGridMonth'
+                    editable={true}
+                    selectable={true}
+                    selectMirror={true}
+                    dayMaxEvents={true}
+                    />
+                    </div>
+            </div>
+    )
 }
