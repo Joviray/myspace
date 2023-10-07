947e873741f8d940c6fdfeb764fd06a6146531c3
Joviray <j3241@live.com>
2023-10-06 20:55:31 -0700

    "demo"

--- app/calendar/page.tsx
+++ app/calendar/page.tsx
@@ -5,6 +5,7 @@ import { formatDate } from '@fullcalendar/core'
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
+import resourceTimeGridPlugin from '@fullcalendar/resource-timegrid'
 
 
 export default function CalendarPage() {
@@ -12,17 +13,24 @@ export default function CalendarPage() {
         <div className='demo-app'>
             <div className='demo-app-main'>
                 <FullCalendar
-                    plugins={[dayGridPlugin, timeGridPlugin]}
+                    plugins={[dayGridPlugin, timeGridPlugin, resourceTimeGridPlugin]}
                     headerToolbar={{
                         left: 'prev,next today',
                         center: 'title',
                         right: 'dayGridMonth,timeGridWeek,timeGridDay'
                     }}
-                    initialView='dayGridMonth'
+                    initialView='resourceTimeGridDay'
                     editable={true}
                     selectable={true}
                     selectMirror={true}
                     dayMaxEvents={true}
+                    resources={[{ id: 'a', title: 'CS Lab', eventColor: 'green'}, 
+                                { id: 'b', title: 'Rm 128E', eventColor: 'blue' }, 
+                                { id: 'c', title: 'Rm 40F', eventColor: 'red' }]}
+                    events={[{ id: '1', resourceId: 'a', start: '2023-10-06T16:00:00', end: '2023-10-06T19:30:00', title: 'ACM Meeting' }, 
+                             { id: '2', resourceId: 'b', start: '2023-10-06T12:00:00', end: '2023-10-06T15:30:00', title: 'Engr study session' },
+                             { id: '3', resourceId: 'c', start: '2023-10-06T10:00:00', end: '2023-10-06T11:30:00', title: 'CS Tutoring' },]}
+
                     />
                     </div>
             </div>
